3ca3f44e9a631d83f84716c1e0e36d1359e2a0e8
Abraham Wari <72644774+abrahamwari@users.noreply.github.com>
2023-04-25 22:40:56 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$db_host = "";
-$db_user = "";
-$db_pass = "";
-$db_name = "";
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_user = "b0d438a744c49b";
+$db_pass = "ab68b02f";
+$db_name = "heroku_7b26d38fd85e699";
 
 $connect = mysqli_connect($db_host, $db_pass, $db_name) or die ("database connection error");
